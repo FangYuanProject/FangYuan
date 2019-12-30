a41386872a018e7fd7a5d9a498537ed2122fed80
tanweicheng <tanweicheng@ourchem.com>
2019-12-30 17:49:40 +0800

    develop

---
+++ src/components/echarts/bar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div :id="id" :style="{width:width,height:height}"></div>
+</template>
+<script>
+const echarts = require('echarts/lib/echarts')
+require('echarts/lib/chart/bar')
+export default {
+  props: {
+    id: {
+      type: String,
+      default: 'eBar'
+    },
+    width: {
+      type: String,
+      default: '400px'
+    },
+    height: {
+      type: String,
+      default: '400px'
+    },
+    options: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  data() {
+    return {
+      data: ''
+    }
+  },
+  mounted() {
+    this.drawBar()
+  },
+  methods: {
+    drawBar() {
+      const chartBar = echarts.init(document.getElementById(this.id))
+      chartBar.setOption({
+        title: { show: false },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/user/analysis.vue
+++ src/views/user/analysis.vue
@@ -1,61 +1,151 @@
 <template>
-  <div>
+  <div class="user-analysis">
     <h2 class="title">用户分析</h2>
-    <tableComponents :table-data="tableData" />
+    <div class="nav">
+      <ul class="tabs">
+        <li :class="{'active': active===1}" @click="chooseTab(1)">用户注册数</li>
+        <li :class="{'active': active===2}" @click="chooseTab(2)">用户登录地区分布</li>
+        <li :class="{'active': active===3}" @click="chooseTab(3)">用户活跃度</li>
+      </ul>
+      <div class="time-picker">
+        <ul class="time-btn">
+          <li :class="{'active':acTime===1}" @click="chooseTime(1,'daterange','yyyy-MM-dd')">日</li>
+          <li :class="{'active':acTime===2}" @click="chooseTime(2,'weekrange','yyyy 第WW周')">周</li>
+          <li :class="{'active':acTime===3}" @click="chooseTime(3,'monthrange','yyyy-MM')">月</li>
+          <li :class="{'active':acTime===4}" @click="chooseTime(4,'yearrange','yyyy')">年</li>
+        </ul>
+        <el-date-picker
+          v-model="timeRange"
+          :type="timeType"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :value-format="valueFormat"
+        />
+      </div>
+    </div>
+    <bar-chart id="bar" width="100%" :option="option" />
   </div>
 </template>
 <script>
-import tableComponents from '@/components/tableComponents'
+import BarChart from '@/components/echarts/bar'
 export default {
   components: {
-    tableComponents
+    BarChart
   },
   data() {
     return {
-      tableData: [
-        {
-          label: '新闻标题ID',
-          prop: '1'
-        },
-        {
-          label: '标题',
-          prop: '1'
-        },
-        {
-          label: '类型',
-          prop: '1'
-        },
-        {
-          label: '新建时间',
-          prop: '1'
-        },
-        {
-          label: '发布时间',
-          prop: '1'
-        },
-        {
-          label: '下架时间',
-          prop: '1'
-        },
-        {
-          label: '状态',
-          prop: '1'
-        },
-        {
-          label: '操作',
-          prop: '1'
-        }
-      ]
+      active: 1,
+      acTime: 1,
+      option: {},
+      timeRange: ['2010-01-01', '2019-12-30'],
+      valueFormat: 'yyyy-MM-dd',
+      timeType: 'daterange'
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    chooseTab(index) {
+      this.active = index
+    },
+    chooseTime(index, type, format) {
+      this.acTime = index
+      this.timeType = type
+      this.valueFormat = format
+      if (index === 1) {
+        this.timeRange = ['2010-01-01', '2019-12-30']
+      } else if (index === 2) {
+        this.timeRange = ['2010 第01周', '2019 第40周']
+      } else if (index === 3) {
+        this.timeRange = ['2010-01', '2019-12']
+      } else {
+        this.timeRange = ['2010', '2019']
+      }
+    },
+    getData() {
+      this.option = {
+        tooltip: { show: false }
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  .user-analysis {
     .title {
-      margin: 26px 0 69px 30px;
+      height: 84px;
+      padding: 0 30px;
+      margin: 0;
       font-size: 20px;
+      line-height: 84px;
       color: #202431;
+      border-bottom: 1px solid #ebeef5;
     }
+
+    .nav {
+      height: 75px;
+      padding: 0;
+      margin: 0 30px;
+      overflow: hidden;
+      border-bottom: 1px solid #ebeef5;
+
+      .tabs {
+        float: left;
+        width: 45%;
+
+        li {
+          display: inline-block;
+          padding: 0 36px;
+          font-size: 16px;
+          font-weight: bold;
+          line-height: 75px;
+          color: #455a64;
+          list-style: none;
+          cursor: pointer;
+
+          &:hover,
+          &.active {
+            color: #0266d6;
+            border-bottom: 2px solid #0266d6;
+          }
+        }
+      }
+
+      .time-picker {
+        float: right;
+        width: 45%;
+
+        .time-btn {
+          display: inline-block;
+          width: 300px;
+          line-height: 75px;
+
+          li {
+            display: inline-block;
+            width: 55px;
+            height: 33px;
+            margin-right: 10px;
+            font-size: 14px;
+            line-height: 33px;
+            color: #455a64;
+            text-align: center;
+            vertical-align: middle;
+            list-style: none;
+            cursor: pointer;
+            border: 1px solid #fff;
+
+            &:hover,
+            &.active {
+              border: 1px solid #455a64;
+              border-radius: 17px;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
 
